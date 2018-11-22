abae73f0bee2541d167ca4fb0f2204fc605d9fa7
Mohammed Alaboud <mohammed.alaboud97@gmail.com>
2018-11-22 18:42:19 +0000

    Created Async controller + updated index
    This resouece: "https://github.com/caolan/async" provides a number of different methods to help execute code asynchronously used in async controller created.
    Included reoutes to async controller in index.

--- controllers/async.Ctrl.js
+++ controllers/async.Ctrl.js
@@ -1,6 +1,44 @@
+//Controller to add all users + notes to front pg
 'use strict';
 
 var Note = require('../models/Note.model');
 var User = require('../models/User.model');
+var async = require('async');
+
+exports.homePage = function(req, res) {
+  // Gather all Notes and all Users
+  async.parallel([
+    function(cb) { //cb for callback
+      var query = Note.find({});
+      query.sort({
+        createdOn: 'desc' //sorted in descending order
+      })
+      .limit(12) //limit number we can see to 12 at once
+      .exec(function(err, notes) { //execute when sorted and limited
+        cb(err, notes); //cus of async we need callback
+      });
+    },
+
+    function(cb) {
+      var query = User.find({});
+      query.sort({ //sorted alphabetically
+        username: 1
+      })
+      .exec(function(err, users) {
+        cb(err, users);
+      });
+    }
+    ],
+    function(err, results) {
+      if(err) { //err in getting user or notes
+        console.log(err);
+      } else {
+        res.render('index', { //render index file
+          notes: results[0], //results is a set of notes or users 
+          users: results[1]
+        });
+      }
+    });
+};
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var notesCtrl = require('../controllers/notes.Ctrl');
+var asyncCtrl = require('../controllers/async.Ctrl');
+
+
+router.get('/', asyncCtrl.homePage); //using this from async instead
+router.post('/', notesCtrl.noteByMember);
 
 // router.get('/', function(req, res) {
 //   res.render('index'); //already defines views in views folder, render index will make express look into views to determine what pg to render
